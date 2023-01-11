606b47373da5685309b3c387b8f3960ac093f1c7
Han H. Pang <hanpang013@gmail.com>
2023-01-11 16:37:28 +0800

    Update SimpleBio.jl

--- src/SimpleBio.jl
+++ src/SimpleBio.jl
@@ -1,18 +1,32 @@
-# __precompile__()
-
 module SimpleBio
 
+
     export 
-    #seqidentiy
-        IsDNA,
-        IsRNA
+        #seqidentiy
+            IsDNA,
+            IsRNA,
 
+        #nucleotidetool
+            Transcript,
+            xTranscript,
+            DNArc,
+            RNArc,
+            RTanscript, 
+        
+        #alignment
+            nAlign,
+            aAlign
 
+    import
+        BioSequences,
+        BioAlignments
     using
-    BioSequences, 
-    BioAlignments
+        BioSequences, 
+        BioAlignments
 
 
+    include("align.jl")
     include("seqidentiy.jl")
+    include("nucleotidetool.jl")
 
 end # module SimpleBio
