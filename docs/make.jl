39de599412f299b2cb2b4ae04d89cf09e71c9928
Han H. Pang <hanpang013@gmail.com>
2023-01-12 17:20:35 +0800

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -19,6 +19,8 @@ makedocs(
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "github.com/doctorbetaq/SimpleBio.jl",
+    devbranch = "main",
+    push_preview = true
+)
