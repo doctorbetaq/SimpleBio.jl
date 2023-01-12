7a30369c1ce750af47bf80981d8eed6f3f56d8e0
Han H. Pang <hanpang013@gmail.com>
2023-01-12 17:31:25 +0800

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -20,7 +20,7 @@ makedocs(
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
 deploydocs(
-    repo = "github.com/doctorbetaq/SimpleBio.jl.git",
+    repo = "github.com/doctorbetaq/SimpleBio.jl",
     devbranch = "main",
     push_preview = true
 )
